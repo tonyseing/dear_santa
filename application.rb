4fdf2cfb06d5e1afd551ef6f845bab9307cfd635
tony <tony@tony-Aspire-M3450.(none)>
2013-10-22 03:23:24 -0400

    bleh

--- application.rb
+++ application.rb
@@ -10,7 +10,6 @@ require 'json'
 
 configure do
   Mongoid.load!('./config/mongoid.yml', :development)
-
 end
 
 email_settings = YAML.load_file(File.open "./config/email.yml")
@@ -18,12 +17,12 @@ email_settings = YAML.load_file(File.open "./config/email.yml")
 
 class Message
   include Mongoid::Document
-
   field :firstname, :type => String
   field :lastname, :type => String
   field :from_email, :type => String
   field :parent_email, :type => String
   field :content, :type => String
+  field :reply, :type => String
 end
 
 
@@ -32,13 +31,14 @@ get '/' do
 end
 
 
-post '/write_santa' do
+post '/santa/write' do
   content_type :json
 
   message = params[:message]
-  if Message.create!(params[:message])
+  message.token = 
+  if message.save!
     Pony.mail({
-                :from => message[:email],
+                :from => "elves@ToSantaWithLove.com",
                 :to => message[:parent_email],
                 :via => :smtp,
                 :via_options => {
@@ -50,8 +50,9 @@ post '/write_santa' do
                 # :cram_md5, no auth by default
                   :domain         => "ToSantaWithLove.com" ,
                 },
+                :subject => "Hello #{message[:parent_email]}, your child #{message[:firstname]} wants to correspond with Santa. Do you want to help your child live out his or her dream?",
 
-                :html_body => "<html><h1>hello world</h1></html>",
+                :html_body => "<html><h1>hello world, go to <a href='http://localhost:9393/santa/reply' ></a></h1></html>",
                 :body => "Yo, your email client can't read html"
 
               
@@ -61,4 +62,12 @@ post '/write_santa' do
   end
 
   { :status => "ok" }.to_json
+end
+
+get '/santa/:id/reply' do
+  erb :reply
+end
+
+post '/santa/:id/reply' do
+  erb :index
 end
