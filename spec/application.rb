d6aaa9264d7a885dfdcbbb7e68ef06d9682a3d3a
tony <tony@tony-Aspire-M3450.(none)>
2013-10-24 04:59:13 -0400

    exhausted

--- spec/application.rb
+++ spec/application.rb
@@ -10,13 +10,28 @@ describe 'The TSWL app' do
   def app
     Sinatra::Application.new
   end
-  
-  it "shows front page" do
-    get '/'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq(File.open("views/index.erb").read)
+
+
+  describe 'GET /' do
+    it "shows front page" do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq(File.open("views/index.erb").read)
+    end
   end
-  
+
+
+  it "shows a page for adults when secret is correct for a message id" do
+    
+  end
+
+  it "redirects adult to an informational page when secret is not correct for a message id" do
+      get '/santa/write/:id'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq(File.open("views/xindex.erb").read)
+      response.should redirect_to "/santa/adults"
+  end
+
 
 end
 
